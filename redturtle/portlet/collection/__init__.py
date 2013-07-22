e58d033b0bc213bd7460e820fa622986c10799d7
Andrea Cecchi <andrea.cecchi85@gmail.com>
2013-07-22 15:47:21 +0200

    added custom permission to add portlet and added upgrade step

--- redturtle/portlet/collection/__init__.py
+++ redturtle/portlet/collection/__init__.py
@@ -1,5 +1,6 @@
 from zope.i18nmessageid import MessageFactory
 RTCollectionPortletMessageFactory = MessageFactory('redturtle.portlet.collection')
 
+
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
---
+++ redturtle/portlet/collection/upgrades.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from Products.CMFCore.utils import getToolByName
+
+default_profile = 'profile-redturtle.portlet.collection:default'
+
+
+def upgrade(upgrade_product, version):
+    """ Decorator for updating the QuickInstaller of a upgrade """
+    def wrap_func(fn):
+        def wrap_func_args(context, *args):
+            p = getToolByName(context, 'portal_quickinstaller').get(upgrade_product)
+            setattr(p, 'installedversion', version)
+            return fn(context, *args)
+        return wrap_func_args
+    return wrap_func
+
+
+@upgrade('redturtle.portlet.collection', '0.4')
+def to_0_4(context):
+    """
+    """
+    context.runImportStepFromProfile(default_profile, 'rolemap')
